4b352112313ffc01e3bd1bebe82daa1c7d09b50f
soe1hom-arch <soe1hom-arch@users.noreply.github.com>
2026-06-29 07:13:35 +0700

    chore: bump version to 2.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "2.0.2"
+        versionName = "2.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
